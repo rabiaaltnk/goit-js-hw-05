d13f3774556e2a4638123038a5a67296dc9b8365
rabia altinok <rabiaaltinok99@icloud.com>
2025-04-20 21:46:38 +0300

    ss

--- js/task-4.js
+++ js/task-4.js
@@ -1,45 +1,46 @@
-const getTotalBalanceByGender = (users, gender) => {
-    return users.reduce((total, user) => {
-      return user.gender === gender ? total + user.balance : total;
-    }, 0);
-  };
 
-  const allUsers = [
-	{
+
+const getTotalBalanceByGender = (users, gender) =>
+  users.reduce(
+    (acc, user) => (user.gender === gender ? acc + user.balance : acc),
+    0
+  );
+const allUsers = [
+  {
     name: "Moore Hensley",
     gender: "male",
-    balance: 2811
+    balance: 2811,
   },
   {
     name: "Sharlene Bush",
     gender: "female",
-    balance: 3821
+    balance: 3821,
   },
   {
     name: "Ross Vazquez",
     gender: "male",
-    balance: 3793
+    balance: 3793,
   },
   {
     name: "Elma Head",
     gender: "female",
-    balance: 2278
+    balance: 2278,
   },
   {
     name: "Carey Barr",
     gender: "male",
-    balance: 3951
+    balance: 3951,
   },
   {
     name: "Blackburn Dotson",
     gender: "male",
-    balance: 1498
+    balance: 1498,
   },
   {
     name: "Sheree Anthony",
     gender: "female",
-    balance: 2764
-  }
+    balance: 2764,
+  },
 ];
 
 console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
